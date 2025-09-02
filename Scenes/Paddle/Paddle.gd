fbc676e45cfd880269a0000c0b8cae77f51c5c7e
Wollex <nisicbogdan@hotmail.com>
2025-09-02 07:43:17 +0200

    Paddle movement changed

--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -1,17 +1,56 @@
 extends Area2D
-
+var snap_target_x := 0.0
 const SPEED: float = 1000
+const SNAP_SPEED: float = 5000  # brzina paddle
 
-# Called when the node enters the scene tree for the first time.
+@onready var holo: Sprite2D = $Holo
+@onready var sprite: Sprite2D = $Sprite2D
+
+var moving_left := false
+var moving_right := false
+var snapping := false
+var holo_active := false
+
 func _ready() -> void:
-	pass
+	holo.z_index = 10
+	holo.visible = false
+	holo.top_level = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var vp = Game.get_vpr()
+	moving_left = Input.is_action_pressed("move_left")
+	moving_right = Input.is_action_pressed("move_right")
+	var direction = int(moving_right) - int(moving_left)
+	var half_width = sprite.texture.get_width() / 2
+	
 
-	var movement: float = Input.get_axis("move_left", "move_right")
-	position.x += SPEED * delta * movement
+	if direction != 0:
+		if not holo_active:
+			holo.global_position = global_position
+			holo.visible = true
+			holo_active = true
+		
+		var new_x = holo.global_position.x + SPEED * delta * direction
+		new_x = clamp(new_x, half_width, vp.size.x - half_width)
+		holo.global_position.x = new_x
+		
+		
 
-	var sprite_half_width = $Sprite2D.texture.get_width() / 2
-	position.x = clampf(position.x, Game.get_vpr().position.x + sprite_half_width, Game.get_vpr().end.x - sprite_half_width)
+		if snapping:
+			snapping = false
+
+	elif direction == 0 and holo_active and not snapping:
+		snapping = true
+		snap_target_x = holo.global_position.x
+		
+
+	if snapping:
+		
+		global_position.x = move_toward(global_position.x, snap_target_x, SNAP_SPEED * delta)
+		global_position.x = clamp(global_position.x, half_width, vp.size.x - half_width)
+		if abs(global_position.x - snap_target_x) <= 1.0:
+			global_position.x = snap_target_x
+			snapping = false
+			holo.visible = false
+			holo_active = false
